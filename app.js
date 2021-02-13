cda6c6d444064ba29cad946f3b62401615f05566
github-admins <bsachita@gmail.com>
2021-02-12 19:35:43 -0500

    made changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Aws Sample code1!'));
+app.get('/', (req, res) => res.send('Aws Sample Full Pipeline code!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
